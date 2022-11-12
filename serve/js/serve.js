7db8c3ed87e1b1d3c2e1308a1b0a527e5a41aa7e
lss55555 <117975630+lss55555@users.noreply.github.com>
2022-11-13 02:46:55 +0800

    Add files via upload

---
+++ serve/js/serve.js
@@ -0,0 +1,79 @@
+// 初始化加载头部尾部
+$(function () {
+    $("#head").load("../public/header.html");
+    $("#footer").load("../public/footer.html");
+})
+$(function () {
+    // 收起下拉菜单
+    $(window).mousemove(function (e) {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var yy = e.pageY;
+        var num;
+        //console.log(scrollTop)
+        //console.log(yy)
+        if (scrollTop > 1) {
+            num = scrollTop + 540
+            if (yy > num) {
+                $("#head").removeClass("active");
+                $("header").removeClass("active");
+            }
+        } else {
+            if (yy > 540) {
+                $("#head").removeClass("active");
+                $("header").removeClass("active");
+            }
+        }
+
+    });
+    //线条
+    $(window).scroll(function () {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if (scrollTop > 1) {
+            $(".b0").removeClass("bShow");
+        } else {
+            $(".b0").addClass("bShow");
+        }
+    });
+    // 点击回到顶部
+	$(window).scroll(function () {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+       if (scrollTop > 500) {
+           $(".backTop").addClass("active");
+       } else {
+           $(".backTop").removeClass("active");
+       }
+   });
+   $(".backTop").click(function () {
+       $('body,html').animate({
+           scrollTop: 0
+       }, 500);
+   })
+   var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: 1.8,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    speed: 500,
+    autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: '.mySwiper1 .swiper-button-next',
+        prevEl: '.mySwiper1 .swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        //当宽度大于等于768
+        768: {
+            slidesPerView: 1.8,
+            spaceBetween: 0
+        },
+    },
+  });
+
+});
